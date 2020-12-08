946df636420f1d9c8f4ea2f3a1bda88d4748cdd5
mokhtartouiri <design.solutions.tn@gmail.com>
2020-12-08 14:33:30 +0000

    dfdfd

--- models/base_model.py
+++ models/base_model.py
@@ -8,9 +8,10 @@ from datetime import datetime
 
 class BaseModel:
     """ PUBLIC """
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """ RETURN STRING """
